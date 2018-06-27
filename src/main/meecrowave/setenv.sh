91a42f500d58829b09bd6ff5d8b434ebe6c857cc
Romain Manni-Bucau <rmannibucau@gmail.com>
2018-06-27 17:49:13 +0200

    fixing .m2 location

--- src/main/meecrowave/setenv.sh
+++ src/main/meecrowave/setenv.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
-export MEECROWAVE_OPTS="$MEECROWAVE_OPTS -Dtalend.component.server.maven.repository=$MEECROWAVE_BASE/.m2"
+export MEECROWAVE_OPTS="$MEECROWAVE_OPTS -Dtalend.component.server.maven.repository=$MEECROWAVE_BASE/.m2/repository"
 export MEECROWAVE_OPTS="$MEECROWAVE_OPTS -Dtalend.component.server.component.coordinates=org.talend:demo:1.0.0"
 export MEECROWAVE_OPTS="$MEECROWAVE_OPTS -Dtalend.component.server.documentation.active=true"
